99847325d094f453b5c825394455a5953ad25e93
penyamoon <147872073+penyamoon@users.noreply.github.com>
2023-12-08 01:06:38 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Phrase = require("./models/phrase");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.post("/login", (req, res) => {
+  Phrase.create(req.body)
+    .then((phrase) => res.json(phrase))
+    .catch((err) => res.json(err));
+});
+
+app.listen(8081, () => {
+  console.log("Listening to 8081");
+});
